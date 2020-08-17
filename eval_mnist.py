822829dc7acd6d9948b7a840baab8a4d53ef3fe7
Yifan Ding <dyf0125@gmail.com>
2020-08-17 16:37:07 -0400

    modify mnist evaluation

--- eval_mnist.py
+++ eval_mnist.py
@@ -1,3 +1,5 @@
+import argparse
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -34,40 +36,65 @@ class MNISTNet(nn.Module):
         return loss if not eval else output, loss
         # return loss
 
-transform=transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize((0.1307,), (0.3081,))
-        ])
-dataset2 = datasets.MNIST('/scratch365/yding4/mnist/MNIST/processed', train=False, download=True,
-                       transform=transform)
+def main(args):
+    transform=transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.1307,), (0.3081,))
+            ])
+    dataset2 = datasets.MNIST(args.mnist_dir, train=False, download=True,
+                           transform=transform)
 
-device = torch.device("cuda")
-checkpoint = torch.load('/scratch365/yding4/hetseq/CRC_RUN_FILE/new_test/node1gpu4/checkpoint_last.pt')
+    device = torch.device("cuda")
+    checkpoint = torch.load(args.model_ckpt)
 
-model = MNISTNet()
-model.load_state_dict(checkpoint['model'])
-model.to(device)
-model.eval()
+    model = MNISTNet()
+    model.load_state_dict(checkpoint['model'])
+    model.to(device)
+    model.eval()
 
-kwargs = {'batch_size': 64}
-test_loader = torch.utils.data.DataLoader(dataset2, **kwargs)
+    kwargs = {'batch_size': 64}
+    test_loader = torch.utils.data.DataLoader(dataset2, **kwargs)
 
 
-test_loss = 0
-correct = 0
-with torch.no_grad():
-    for data, target in test_loader:
-        data, target = data.to(device), target.to(device)
-        outputs = model(data, target, eval=True)
-        output = outputs[0]
-        loss = outputs[1]
-        test_loss += loss.item()
-        pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
-        correct += pred.eq(target.view_as(pred)).sum().item()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            outputs = model(data, target, eval=True)
+            output = outputs[0]
+            loss = outputs[1]
+            test_loss += loss.item()
+            pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+            correct += pred.eq(target.view_as(pred)).sum().item()
 
-test_loss /= len(test_loader.dataset)
+    test_loss /= len(test_loader.dataset)
 
-print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
-    test_loss, correct, len(test_loader.dataset),
-    100. * correct / len(test_loader.dataset)))
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
 
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='evaluate trained mnist model',
+        allow_abbrev=False,
+    )
+
+    parser.add_argument(
+        '--model_ckpt',
+        type=str,
+        default='/scratch365/yding4/hetseq/CRC_RUN_FILE/new_test/node1gpu4/checkpoint_last.pt',
+        help='Specify the input AIDA directory',
+    )
+
+    parser.add_argument(
+        '--mnist_dir',
+        type=str,
+        default='/scratch365/yding4/mnist/MNIST/processed',
+        help='Specify the input AIDA directory',
+    )
+
+
+    args = parser.parse_args()
+    main(args)
