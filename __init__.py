79fda6fb453868407acb230afa444c5464272df3
Yifan Ding <dyf0125@gmail.com>
2020-12-18 11:21:25 -0500

    delete original files

--- __init__.py
+++
@@ -1,2 +0,0 @@
-import hetseq.data
-import hetseq.tasks
--- data/__init__.py
+++
@@ -1,8 +0,0 @@
-from .h5pyDataset import BertH5pyData, ConBertH5pyData
-from .mnist_dataset import MNISTDataset
-
-__all__ = [
-    'BertH5pyData',
-    'ConBertH5pyData',
-    'MNISTDataset',
-]
