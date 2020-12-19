8401623a05f9e22ddb5d8a6ebf38be523a9778ba
Yifan Ding <dyf0125@gmail.com>
2020-12-19 12:39:28 -0500

    add bert_ner_dataset in __init__

--- hetseq/data/__init__.py
+++ hetseq/data/__init__.py
@@ -1,8 +1,10 @@
 from .h5pyDataset import BertH5pyData, ConBertH5pyData
 from .mnist_dataset import MNISTDataset
+from .bert_ner_dataset import BertNerDataset
 
 __all__ = [
     'BertH5pyData',
     'ConBertH5pyData',
     'MNISTDataset',
+    'BertNerDataset',
 ]
