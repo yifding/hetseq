0a1be4e71140c9ea9afff6bdd1fba8891baed5e1
Yifan Ding <dyf0125@gmail.com>
2020-12-19 03:15:47 -0500

    debug conll2003 bert_ner done

--- test/test_conll_2003_dataset.py
+++ test/test_conll_2003_dataset.py
@@ -9,6 +9,12 @@ from hetseq.bert_modeling import (
     BertForTokenClassification,
 )
 
+from transformers import (
+    BertTokenizerFast,
+    DataCollatorForTokenClassification,
+)
+
+import torch
 from torch.utils.data.dataloader import DataLoader
 
 _NER_COLUMNS = ['input_ids', 'labels', 'token_type_ids', 'attention_mask']
@@ -55,6 +61,41 @@ def main(args):
 
     args.num_labels = len(label_list)
 
+    # Tokenize all texts and align the labels with them.
+    def tokenize_and_align_labels(examples, label_all_tokens=False):
+        tokenized_inputs = tokenizer(
+            examples[text_column_name],
+            padding=False,
+            truncation=True,
+            # We use this argument because the texts in our dataset are lists of words (with a label for each word).
+            is_split_into_words=True,
+            return_offsets_mapping=True,
+        )
+        offset_mappings = tokenized_inputs.pop("offset_mapping")
+        labels = []
+        for label, offset_mapping in zip(examples[label_column_name], offset_mappings):
+            label_index = 0
+            current_label = -100
+            label_ids = []
+            for offset in offset_mapping:
+                # We set the label for the first token of each word. Special characters will have an offset of (0, 0)
+                # so the test ignores them.
+                if offset[0] == 0 and offset[1] != 0:
+                    current_label = label_to_id[label[label_index]]
+                    label_index += 1
+                    label_ids.append(current_label)
+                # For special tokens, we set the label to -100 so it's automatically ignored in the loss function.
+                elif offset[0] == 0 and offset[1] == 0:
+                    label_ids.append(-100)
+                # For the other tokens in a word, we set the label to either the current label or -100, depending on
+                # the label_all_tokens flag.
+                else:
+                    label_ids.append(current_label if label_all_tokens else -100)
+
+            labels.append(label_ids)
+        tokenized_inputs["labels"] = labels
+        return tokenized_inputs
+
     tokenized_datasets = dataset.map(
         tokenize_and_align_labels,
         batched=True,
@@ -70,11 +111,13 @@ def main(args):
     # **YD** dataloader
     data_loader = DataLoader(
         train_dataset,
-        batch_size=batch_size,
-        sampler=train_sampler,
+        batch_size=args.batch_size,
+        #sampler=train_sampler,
+        sampler=None,
         collate_fn=data_collator,
         drop_last=False,
-        num_workers=8,
+        # num_workers=8,
+        num_workers=0,
     )
 
     # 3. prepare bert-model loading pre-trained checkpoint
@@ -82,48 +125,14 @@ def main(args):
     model = prepare_model(args)
 
     for index, input in enumerate(data_loader):
-        model(**input)
-
-
-# Tokenize all texts and align the labels with them.
-def tokenize_and_align_labels(examples, label_all_tokens=False):
-    tokenized_inputs = tokenizer(
-        examples[text_column_name],
-        padding=False,
-        truncation=True,
-        # We use this argument because the texts in our dataset are lists of words (with a label for each word).
-        is_split_into_words=True,
-        return_offsets_mapping=True,
-    )
-    offset_mappings = tokenized_inputs.pop("offset_mapping")
-    labels = []
-    for label, offset_mapping in zip(examples[label_column_name], offset_mappings):
-        label_index = 0
-        current_label = -100
-        label_ids = []
-        for offset in offset_mapping:
-            # We set the label for the first token of each word. Special characters will have an offset of (0, 0)
-            # so the test ignores them.
-            if offset[0] == 0 and offset[1] != 0:
-                current_label = label_to_id[label[label_index]]
-                label_index += 1
-                label_ids.append(current_label)
-            # For special tokens, we set the label to -100 so it's automatically ignored in the loss function.
-            elif offset[0] == 0 and offset[1] == 0:
-                label_ids.append(-100)
-            # For the other tokens in a word, we set the label to either the current label or -100, depending on
-            # the label_all_tokens flag.
-            else:
-                label_ids.append(current_label if label_all_tokens else -100)
-
-        labels.append(label_ids)
-    tokenized_inputs["labels"] = labels
-    return tokenized_inputs
+        print(model(**input))
+        break
 
 def prepare_tokenizer(args):
     config = BertConfig.from_json_file(args.config_file)
     # tokenizer = BertTokenizerFast(args.vocab_file, model_max_length=512)
-    tokenizer = BertTokenizerFast(args.vocab_file, model_max_length=config.model_max_length)
+    # print('config', type(config), config,)
+    tokenizer = BertTokenizerFast(args.vocab_file, model_max_length=config.max_position_embeddings)
     return tokenizer
 
 
@@ -132,7 +141,7 @@ def prepare_model(args):
     model = BertForTokenClassification(config, args.num_labels)
     if args.hetseq_state_dict != '':
         # load hetseq state_dictionary
-        model.load_state_dict(torch.load(args.hetseq_state_dict, map_location='cpu'))
+        model.load_state_dict(torch.load(args.hetseq_state_dict, map_location='cpu')['model'], strict=False)
 
     return model
 
@@ -200,6 +209,13 @@ def cli_main():
         help='hetseq pre-trained state dictionary ',
         type=str,
         default='',
+    )
+
+    parser.add_argument(
+        '--batch_size',
+        help='batch size for data_loader',
+        type=int,
+        default=8,
     )
 
     args = parser.parse_args()
