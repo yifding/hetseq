744df9faa6a8d5dce786d31fded96adc9cea43bd
Yifan Ding <dyf0125@gmail.com>
2020-12-26 17:17:46 -0500

    add eval_bert_fine_tuning

---
+++ hetseq/eval_bert_fine_tuning_ner.py
@@ -0,0 +1,9 @@
+__DATE__ = '2020/12/26'
+__AUTHOR__ = 'Yifan_Ding'
+__E_MAIL = 'yding4@nd.edu'
+
+# 1. load dataset, either from CONLL2003 or other entity linking datasets.
+# 2. load model from a trained one, load model architecture and state_dict from a trained one.
+# 3. predict loss, generate predicted label
+# 4. compare predicted label with ground truth label to obtain evaluation results.
+
